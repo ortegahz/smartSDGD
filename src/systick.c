01268e5b66fd193c9a909a7c10325bee4a0c7d4f
manu <zxthz@126.com>
2024-03-28 16:52:36 +0800

    tiny

--- src/systick.c
+++ src/systick.c
@@ -1,36 +1,11 @@
-/*!
-    \file    systick.c
-    \brief   the systick configuration file
-
-    \version 2023-06-28, V1.1.3, demo for GD32W51x
-*/
-
 /*
-    Copyright (c) 2023, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this 
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
-OF SUCH DAMAGE.
-*/
+ * @Author: manu zxthz@126.com
+ * @Date: 2024-03-28 15:18:22
+ * @LastEditors: manu zxthz@126.com
+ * @LastEditTime: 2024-03-28 16:51:38
+ * @FilePath: /smartSDGD/src/systick.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 #include "gd32w51x.h"
 #include "systick.h"
@@ -46,9 +21,11 @@ volatile static uint32_t delay;
 void systick_config(void)
 {
     /* setup systick timer for 1000Hz interrupts */
-    if(SysTick_Config(SystemCoreClock / 1000U)){
+    if (SysTick_Config(SystemCoreClock / 1000U))
+    {
         /* capture error */
-        while(1){
+        while (1)
+        {
         }
     }
     /* configure the systick handler priority */
@@ -64,7 +41,8 @@ void systick_config(void)
 void delay_1ms(uint32_t count)
 {
     delay = count;
-    while(0U != delay){
+    while (0U != delay)
+    {
     }
 }
 
@@ -76,7 +54,8 @@ void delay_1ms(uint32_t count)
 */
 void delay_decrement(void)
 {
-    if(0U != delay){
+    if (0U != delay)
+    {
         delay--;
     }
 }
